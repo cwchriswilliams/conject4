747dace0860aef82b4ecc1731b92a5e9002baee8
Chris Williams <cw.chris.williams@gmail.com>
2021-10-09 12:31:39 +0100

    Replaced with index-based grid

--- src/conject4/core.clj
+++ src/conject4/core.clj
@@ -7,48 +7,75 @@
   (println "Hello, World!"))
 
 (defn create-empty-board [x-size y-size]
-  (vec (repeat y-size (vec (repeat x-size :empty)))))
+  {:height y-size :width x-size :board {}})
 
 (defn get-board-height [board]
-  (count board))
+  (:height board))
 
 (defn get-board-width [board]
-  (count (first board)))
+  (:width board))
+
+(defn get-position-index [board x-pos y-pos]
+  (+ (* y-pos (get-board-width board)) x-pos))
 
 (defn get-piece-in-position [board x-pos y-pos]
-  (get (get (vec (reverse board)) y-pos) x-pos)
+  (get (:board board) (get-position-index board x-pos y-pos) :empty)
   )
 
 (defn is-piece-in-position? [board x-pos y-pos piece]
-  (= (get-piece-in-position board x-pos y-pos) piece)
-  )
+  (= (get-piece-in-position board x-pos y-pos) piece))
 
 (defn is-position-empty? [board x-pos y-pos]
   (is-piece-in-position? board x-pos y-pos :empty))
+
+(def is-position-filled? (complement is-position-empty?))
+
 
 (defn is-position-valid? [board x-pos]
   (not
     (or
       (neg? x-pos)
       (>= x-pos (get-board-width board))
-      (not= :empty (get (get board 0) x-pos))
+      (is-position-filled? board x-pos (dec (get-board-height board)))
       )
     )
   )
 
 (defn place-counter [board x-pos y-pos counter-colour]
-  (assoc board (- (get-board-height board) y-pos 1) (assoc (get board x-pos) x-pos counter-colour))
+  (if (= counter-colour :empty)
+    (update-in board [:board] dissoc (get-position-index board x-pos y-pos))
+    (assoc-in board [:board (get-position-index board x-pos y-pos)] counter-colour))
   )
 
 (defn get-lowest-empty-in-column [board x-pos]
   (loop [current-row-index 0]
     (if (is-position-empty? board x-pos current-row-index)
       current-row-index
-      (recur (inc current-row-index))))
-  )
+      (recur (inc current-row-index)))))
 
 (defn apply-move [board x-pos counter-colour]
   (if (is-position-valid? board x-pos)
     {:board (place-counter board x-pos (get-lowest-empty-in-column board x-pos) counter-colour) :is-valid-move true}
     {:board board :is-valid-move false}
     ))
+
+(defn print-board [board]
+  (loop [board board
+         current-idx 0
+         printable-board []
+         board-width (get-board-width board)
+         max-idx (* (get-board-height board) board-width)]
+    (if (= current-idx max-idx)
+      (vec (reverse printable-board))
+      (if (= (mod current-idx board-width) 0)
+        (recur board
+               (inc current-idx)
+               (conj printable-board [(get (:board board) current-idx :empty)])
+               board-width
+               max-idx)
+        (recur board
+               (inc current-idx)
+               (update printable-board (- (count printable-board) 1) #(conj % (get (:board board) current-idx :empty)))
+               board-width
+               max-idx))))
+  )
--- test/conject4/core_test.clj
+++ test/conject4/core_test.clj
@@ -2,143 +2,117 @@
   (:require [clojure.test :refer :all]
             [conject4.core :as sut]))
 
+(def test-6x5-empty-board {:width 6 :height 5 :board {}})
+(def test-6x5-laid-board  {:width 6 :height 5 :board {0 :red 2 :yellow 5 :red 6 :yellow 7 :yellow}})
+(def test-4x5-laid-board  {:width 4 :height 5 :board {0 :red 2 :yellow 5 :red 6 :yellow 7 :yellow}})
+(def test-4x3-laid-board  {:width 4 :height 3 :board {0 :red 4 :yellow 8 :red}})
+
+(sut/print-board test-4x3-laid-board)
+
 (deftest test-board-creation
-  "default board is empty regardless of size"
-  (is (= (sut/create-empty-board 1 1) [[:empty]]))
-  (is (= (sut/create-empty-board 1 2) [[:empty] [:empty]]))
-  (is (= (sut/create-empty-board 1 3) [[:empty] [:empty] [:empty]]))
-  (is (= (sut/create-empty-board 2 1) [[:empty :empty]]))
-  (is (= (sut/create-empty-board 3 1) [[:empty :empty :empty]]))
-  (is (= (sut/create-empty-board 2 2) [[:empty :empty] [:empty :empty]]))
+  "default num board is empty regardless of size"
+  (is (= (sut/create-empty-board 1 1) {:width 1 :height 1 :board {}}))
+  (is (= (sut/create-empty-board 1 2) {:width 1 :height 2 :board {}}))
+  (is (= (sut/create-empty-board 1 3) {:width 1 :height 3 :board {}}))
+  (is (= (sut/create-empty-board 2 1) {:width 2 :height 1 :board {}}))
+  (is (= (sut/create-empty-board 3 1) {:width 3 :height 1 :board {}}))
+  (is (= (sut/create-empty-board 2 2) {:width 2 :height 2 :board {}}))
   )
 
-(def test-board [[:empty :empty :empty :empty]
-                 [:empty :empty :empty :empty]
-                 [:empty :empty :empty :empty]
-                 [:empty :empty :empty :empty]
-                 [:empty :empty :empty :empty]])
-
-(def test-board-with-red-column [[:empty :red :empty :empty]
-                                  [:empty :red :empty :empty]
-                                  [:empty :red :empty :empty]
-                                  [:empty :red :empty :empty]
-                                  [:empty :red :empty :empty]])
-
-(def test-board-with-yellow-column [[:empty :empty :yellow :empty]
-                                  [:empty :empty :yellow :empty]
-                                  [:empty :empty :yellow :empty]
-                                  [:empty :empty :yellow :empty]
-                                  [:empty :empty :yellow :empty]])
-
-(def test-board-with-red-row [[:empty :empty :empty :empty]
-                 [:red :red :red :red]
-                 [:empty :empty :empty :empty]
-                 [:empty :empty :empty :empty]
-                 [:empty :empty :empty :empty]])
-
-(def test-board-with-yellow-row [[:empty :empty :empty :empty]
-                 [:empty :empty :empty :empty]
-                 [:yellow :yellow :yellow :yellow]
-                 [:empty :empty :empty :empty]
-                 [:empty :empty :empty :empty]])
+(deftest print-board
+  "Prints board as expected"
+  (is (= (sut/print-board {:height 2 :width 4 :board {}}) [[:empty :empty :empty :empty]
+                                                               [:empty :empty :empty :empty]]))
+  (is (= (sut/print-board {:height 2 :width 4 :board {0 :red 2 :yellow 5 :red 6 :yellow 7 :yellow}}) [[:empty :red :yellow :yellow]
+                                                                                                          [:red :empty :yellow :empty]]))
+  )
 
 (deftest test-get-piece-in-position
   "returns empty if space is empty"
-  (is (= (sut/get-piece-in-position test-board-with-red-column 2 2) :empty))
-  (is (= (sut/get-piece-in-position test-board-with-yellow-column 1 2) :empty))
-  (is (= (sut/get-piece-in-position test-board-with-red-row 2 2) :empty))
-  (is (= (sut/get-piece-in-position test-board-with-yellow-row 0 0) :empty))
+  (is (= (sut/get-piece-in-position test-6x5-empty-board 2 2) :empty))
+  (is (= (sut/get-piece-in-position test-6x5-laid-board 1 2) :empty))
+  (is (= (sut/get-piece-in-position test-6x5-laid-board 2 2) :empty))
+  (is (= (sut/get-piece-in-position test-6x5-laid-board 1 0) :empty))
   "returns correct contents of space if space is not empty"
-  (is (= (sut/get-piece-in-position test-board-with-red-column 1 2) :red))
-  (is (= (sut/get-piece-in-position test-board-with-yellow-column 2 2) :yellow))
-  (is (= (sut/get-piece-in-position test-board-with-red-row 2 3) :red))
-  (is (= (sut/get-piece-in-position test-board-with-yellow-row 2 2) :yellow))
+  (is (= (sut/get-piece-in-position test-6x5-laid-board 0 0) :red))
+  (is (= (sut/get-piece-in-position test-6x5-laid-board 2 0) :yellow))
+  (is (= (sut/get-piece-in-position test-6x5-laid-board 5 0) :red))
+  (is (= (sut/get-piece-in-position test-6x5-laid-board 0 1) :yellow))
+  (is (= (sut/get-piece-in-position test-6x5-laid-board 1 1) :yellow))
+  (is (= (sut/get-piece-in-position test-4x5-laid-board 3 1) :yellow))
   )
+
 
 (deftest test-is-piece-in-position
   "returns true if expected piece in position"
-  (is (true? (sut/is-piece-in-position? test-board-with-red-row 0 1 :empty)))
-  (is (true? (sut/is-piece-in-position? test-board-with-red-row 0 3 :red)))
-  (is (true? (sut/is-piece-in-position? test-board-with-yellow-row 3 2 :yellow)))
+  (is (true? (sut/is-piece-in-position? test-6x5-laid-board 0 0 :red)))
+  (is (true? (sut/is-piece-in-position? test-6x5-laid-board 2 0 :yellow)))
+  (is (true? (sut/is-piece-in-position? test-4x5-laid-board 3 1 :yellow)))
+  (is (true? (sut/is-piece-in-position? test-4x5-laid-board 0 1 :empty)))
   "returns false if expected piece not in position"
-  (is (false? (sut/is-piece-in-position? test-board-with-red-row 0 3 :empty)))
-  (is (false? (sut/is-piece-in-position? test-board-with-red-row 0 1 :red)))
-  (is (false? (sut/is-piece-in-position? test-board-with-yellow-row 3 1 :yellow)))
+  (is (false? (sut/is-piece-in-position? test-6x5-laid-board 0 0 :empty)))
+  (is (false? (sut/is-piece-in-position? test-6x5-laid-board 2 0 :red)))
+  (is (false? (sut/is-piece-in-position? test-4x5-laid-board 0 1 :yellow)))
   )
 
 (deftest test-is-position-empty?
   "returns true if position is empty"
-  (is (true? (sut/is-position-empty? test-board-with-red-row 0 1)))
+  (is (true? (sut/is-position-empty? test-4x5-laid-board 0 1)))
+  (is (true? (sut/is-position-empty? test-4x3-laid-board 1 2)))
   "returns false if position is not empty"
-  (is (false? (sut/is-position-empty? test-board-with-red-row 0 3)))
-  (is (false? (sut/is-position-empty? test-board-with-yellow-row 3 2)))
+  (is (false? (sut/is-position-empty? test-4x5-laid-board 3 1)))
+  (is (false? (sut/is-position-empty? test-6x5-laid-board 2 0)))
+  )
+
+(deftest test-is-position-filled?
+  "returns true if position is empty"
+  (is (false? (sut/is-position-filled? test-4x5-laid-board 0 1)))
+  (is (false? (sut/is-position-filled? test-4x3-laid-board 1 2)))
+  "returns false if position is not empty"
+  (is (true? (sut/is-position-filled? test-4x5-laid-board 3 1)))
+  (is (true? (sut/is-position-filled? test-6x5-laid-board 2 0)))
+  (is (true? (sut/is-position-filled? test-4x3-laid-board 0 2)))
   )
 
 (deftest test-is-position-valid?
   "is-position-valid? returns false for negative numbers"
-  (is (false? (sut/is-position-valid? test-board -1)))
-  (is (false? (sut/is-position-valid? test-board -50)))
+  (is (false? (sut/is-position-valid? test-6x5-laid-board -1)))
+  (is (false? (sut/is-position-valid? test-6x5-laid-board -50)))
   "is-position-valid? returns false for numbers bigger than the board (-1 for 0-indexing)"
-  (is (false? (sut/is-position-valid? test-board 5)))
-  (is (false? (sut/is-position-valid? test-board 6)))
-  (is (false? (sut/is-position-valid? test-board 60)))
+  (is (false? (sut/is-position-valid? test-6x5-laid-board 6)))
+  (is (false? (sut/is-position-valid? test-6x5-laid-board 7)))
+  (is (false? (sut/is-position-valid? test-6x5-laid-board 60)))
+  (is (false? (sut/is-position-valid? test-4x3-laid-board 4)))
   "is-position-valid? returns true for numbers within the board (-1 for 0-indexing)"
-  (is (true? (sut/is-position-valid? test-board 2)))
-  (is (true? (sut/is-position-valid? test-board 3)))
-  (is (true? (sut/is-position-valid? test-board 0)))
+  (is (true? (sut/is-position-valid? test-4x3-laid-board 1)))
+  (is (true? (sut/is-position-valid? test-4x3-laid-board 2)))
   "is-position-valid? returns false for when the column is full"
-  (is (false? (sut/is-position-valid? test-board-with-red-column 1)))
-  (is (false? (sut/is-position-valid? test-board-with-yellow-column 2)))
+  (is (false? (sut/is-position-valid? test-4x3-laid-board 0)))
   )
 
 (deftest test-place-counter
-  "places in the location specified"
-  (is (= (sut/place-counter test-board 1 3 :red) [[:empty :empty :empty :empty]
-                                                  [:empty :red :empty :empty]
-                                                  [:empty :empty :empty :empty]
-                                                  [:empty :empty :empty :empty]
-                                                  [:empty :empty :empty :empty]]))
-  (is (= (sut/place-counter test-board 1 3 :yellow) [[:empty :empty :empty :empty]
-                                                  [:empty :yellow :empty :empty]
-                                                  [:empty :empty :empty :empty]
-                                                  [:empty :empty :empty :empty]
-                                                  [:empty :empty :empty :empty]]))
-  (is (= (sut/place-counter test-board 1 3 :empty) [[:empty :empty :empty :empty]
-                                                  [:empty :empty :empty :empty]
-                                                  [:empty :empty :empty :empty]
-                                                  [:empty :empty :empty :empty]
-                                                  [:empty :empty :empty :empty]]))
+  "Places in the location specified on empty board"
+  (is (= (sut/place-counter test-6x5-empty-board 0 0 :red) {:width 6 :height 5 :board {0 :red}}))
+  (is (= (sut/place-counter test-6x5-empty-board 1 0 :yellow) {:width 6 :height 5 :board {1 :yellow}}))
+  (is (= (sut/place-counter test-6x5-empty-board 0 1 :yellow) {:width 6 :height 5 :board {6 :yellow}}))
+  "Places in the location specified on non-empty board"
+  (is (= (sut/place-counter test-4x3-laid-board 1 1 :yellow) {:width 4 :height 3 :board {0 :red 4 :yellow 8 :red 5 :yellow}}))
+  (is (= (sut/place-counter test-4x3-laid-board 2 1 :yellow) {:width 4 :height 3 :board {0 :red 4 :yellow 8 :red 6 :yellow}}))
+  "Replaces the piece in position"
+  (is (= (sut/place-counter test-4x3-laid-board 0 0 :yellow) {:width 4 :height 3 :board {0 :yellow 4 :yellow 8 :red}}))
+  (is (= (sut/place-counter test-4x3-laid-board 4 0 :red) {:width 4 :height 3 :board {0 :red 4 :red 8 :red}}))
+  "Removes the piece if empty selected"
+  (is (= (sut/place-counter test-4x3-laid-board 0 0 :empty) {:width 4 :height 3 :board {4 :yellow 8 :red}}))
+  (is (= (sut/place-counter test-4x3-laid-board 4 0 :empty) {:width 4 :height 3 :board {0 :red 8 :red}}))
   )
 
 (deftest test-apply-move
   "apply-move returns map with is-valid set to false if move is not valid"
-  (is (false? (:is-valid-move (sut/apply-move test-board -1 :red))))
+  (is (false? (:is-valid-move (sut/apply-move test-4x3-laid-board -1 :red))))
   "apply-move returns map with board set to input board if move is not valid"
-  (is (= (:board (sut/apply-move test-board -1 :red)) test-board))
+  (is (= (:board (sut/apply-move test-4x3-laid-board -1 :red)) test-4x3-laid-board))
   "apply-move returns map with is-valid set to true if move is valid"
-  (is (true? (:is-valid-move (sut/apply-move test-board 3 :red))))
-  "apply-move returns map with board set to correct value if move is valid"
-  (is (= (:board (sut/apply-move test-board 1 :red)) [[:empty :empty :empty :empty]
-                                                      [:empty :empty :empty :empty]
-                                                      [:empty :empty :empty :empty]
-                                                      [:empty :empty :empty :empty]
-                                                      [:empty :red :empty :empty]]))
-  (is (= (:board (sut/apply-move [[:empty :empty :empty :empty]
-                                  [:empty :empty :empty :empty]
-                                  [:empty :empty :empty :empty]
-                                  [:empty :empty :empty :empty]
-                                  [:empty :red :empty :empty]] 1 :red)) [[:empty :empty :empty :empty]
-                                                                         [:empty :empty :empty :empty]
-                                                                         [:empty :empty :empty :empty]
-                                                                         [:empty :red :empty :empty]
-                                                                         [:empty :red :empty :empty]]))
-  (is (= (:board (sut/apply-move [[:empty :empty :empty :empty]
-                                  [:empty :empty :empty :empty]
-                                  [:empty :empty :empty :empty]
-                                  [:empty :red :empty :empty]
-                                  [:empty :red :empty :empty]] 1 :yellow)) [[:empty :empty :empty :empty]
-                                                                            [:empty :empty :empty :empty]
-                                                                            [:empty :yellow :empty :empty]
-                                                                            [:empty :red :empty :empty]
-                                                                            [:empty :red :empty :empty]]))
+  (is (true? (:is-valid-move (sut/apply-move test-4x3-laid-board 1 :red))))
+  "apply move returns map with board set to the updated board if move is valid"
+  (is (= (:board (sut/apply-move test-4x3-laid-board 1 :red)) {:width 4 :height 3 :board {0 :red 1 :red 4 :yellow 8 :red}}))
   )
